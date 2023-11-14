bf92548e873a89c2fa6aa2b1e282fdb7eebd4a8d
Dmytro Klymchuk <dima.klimchuk.2016@gmail.com>
2023-11-14 23:50:58 +0200

    Working with api and routing

--- Node Farm/index.js
+++ Node Farm/index.js
@@ -29,14 +29,30 @@ const url = require("url");
 
 
 // Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-    if(pathName === '/' || pathName === '/overview'){
+    // Overview page
+    if (pathName === '/' || pathName === '/overview'){
         res.end('This is the OVERVIEW');
+
+    // Product page
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT');
-    }else{
+    
+    // API
+    }else if (pathName === '/api'){
+
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data);
+
+    // Not found  
+    } else{
         res.writeHead(404, {
             'Content-type' : 'text/html',
             'my-own-header' : 'hello-world'
